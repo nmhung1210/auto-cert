12a408d212323b19880d41949b7eeb0ef0dc0703
nmhung1210 <nmhung1210@gmail.com>
2019-09-06 17:49:56 +0700

    Update readme. Fix validation issue

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ It's a standalone http server that handling letsencrypt challenging and forward 
 ## Scripts usage (at your server)
 
 ```
-docker run -d -v /etc/letsencrypt:/etc/letsencrypt /var/lib/letsencrypt:/var/lib/letsencrypt -p 80:80 nmhung1210/auto-cert
+docker run -d -v /etc/letsencrypt:/etc/letsencrypt -v /var/lib/letsencrypt:/var/lib/letsencrypt -p 80:80 nmhung1210/auto-cert
 ```
 
 ## To init cert for a domain
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const execSync = require('child_process').execSync;
+const express = require("express");
+const execSync = require("child_process").execSync;
+const readFileSync = require("fs").readFileSync;
 
 const app = express();
 const tracks = {};
@@ -11,27 +12,35 @@ const renews = () => {
   renewsPid = setTimeout(renews, 24 * 60 * 60 * 1000);
 };
 
-app.all('/.well-known/*', express.static('/tmp'));
-app.use('/.ssl/init', function(req, res) {
+app.get("/.well-known/acme-challenge/:token", (req, res) => {
+  const { token } = req.params;
+  const validation = readFileSync(
+    `/tmp/.well-known/acme-challenge/${token.trim()}`
+  );
+  res.send(validation);
+});
+
+app.use("/.ssl/init", function(req, res) {
   if (tracks[req.hostname]) {
-    return req.send('ok');
+    return req.send("ok");
   }
   try {
     execSync(
       `certbot certonly --agree-tos --email=nmhung1210@gmail.com -n --webroot -w /tmp -d ${req.hostname}`,
       {
-        stdio: 'inherit'
+        stdio: "inherit"
       }
     );
     renews();
     tracks[req.hostname] = Date.now();
-    res.send('ok');
+    res.send("ok");
   } catch (error) {
-    res.send('error!');
+    res.send("error!");
   }
 });
-app.use(function(req, res) {
-  return res.redirect('https://' + req.headers.host + req.url);
+
+app.use((req, res) => {
+  return res.redirect("https://" + req.headers.host + req.url);
 });
 
 app.listen(80);
