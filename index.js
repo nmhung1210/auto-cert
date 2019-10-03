148abc438c5fd29537fbd37ac4deccbbb110b39a
nmhung1210 <nmhung1210@gmail.com>
2019-10-04 04:22:23 +0700

    Adding support for aws instance

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const exec = require("child_process").exec;
+const express = require('express');
+const exec = require('child_process').exec;
 
 const app = express();
 const tracks = {};
@@ -11,32 +11,56 @@ const renews = () => {
   renewsPid = setTimeout(renews, 24 * 60 * 60 * 1000);
 };
 
-app.get("/.well-known/*", express.static("/tmp"));
-app.use("/.ssl/init", function(req, res) {
-  if (tracks[req.hostname]) {
-    return req.send("ok");
+const initSSL = async hostname => {
+  if (tracks[hostname]) {
+    return 'OK';
   }
-  try {
-    exec(
-      `certbot certonly --agree-tos --email=nmhung1210@gmail.com -n --webroot -w /tmp -d ${req.hostname}`,
-      (err, stdout, stderr) => {
-        if (err) {
-          console.log({ err, stdout, stderr });
-          res.send("error!");
-        } else {
-          renews();
-          tracks[req.hostname] = Date.now();
-          res.send("ok");
+  return new Promise((resolve, reject) => {
+    try {
+      exec(
+        `certbot certonly --agree-tos --email=nmhung1210@gmail.com -n --webroot -w /tmp -d ${hostname}`,
+        (err, stdout, stderr) => {
+          if (err) {
+            console.log({ err, stdout, stderr });
+            reject('failed!');
+          } else {
+            renews();
+            tracks[hostname] = Date.now();
+            resolve('OK');
+          }
         }
-      }
-    );
+      );
+    } catch (error) {
+      reject('failed!');
+    }
+  });
+};
+
+const initAWSHostname = async () => {
+  try {
+    const result = await fetch('http://169.254.169.254/latest/meta-data/public-hostname');
+    const hostname = await result.text();
+    const sslResult = await initSSL(hostname);
+    console.log(`initAWSHostname ${hostname}=${sslResult}`);
   } catch (error) {
-    res.send("error!");
+    console.log('Not a AWS instance!');
+  }
+};
+
+app.get('/.well-known/*', express.static('/tmp'));
+app.use('/.ssl/init', async (req, res) => {
+  try {
+    const result = await initSSL(req.hostname);
+    res.send(result);
+  } catch (error) {
+    res.send('Failed!');
   }
 });
 
 app.use((req, res) => {
-  return res.redirect("https://" + req.headers.host + req.url);
+  return res.redirect('https://' + req.headers.host + req.url);
 });
+
+initAWSHostname();
 
 app.listen(80);
